d4cc39546e3099540aa861446609b9a7d8c2fd07
lettergram <austin@agw.io>
2023-04-04 23:50:53 -0500

    default empty array

--- db/migrate/20230326042755_add_target_positions_to_letters.rb
+++ db/migrate/20230326042755_add_target_positions_to_letters.rb
@@ -1,5 +1,5 @@
 class AddTargetPositionsToLetters < ActiveRecord::Migration[7.0]
   def change
-    add_column :letters, :target_positions, :string, array: true, default: "", null: false
+    add_column :letters, :target_positions, :string, array: true, default: [], null: false
   end
 end
