e32882a59c8fa6e5226957710df6db68a801c307
lettergram <austin@agw.io>
2023-04-15 23:50:05 -0500

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,11 @@ class UsersController < ApplicationController
   
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
-
     respond_to do |format|
       format.html
       format.pdf do
