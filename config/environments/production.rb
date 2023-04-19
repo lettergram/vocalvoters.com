f2445555b0a51f71fc2ba465833f9a0fca2eb2fa
lettergram <austin@agw.io>
2023-04-19 16:43:51 -0500

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,19 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Default Mailer Host
-  Rails.application.routes.default_url_options[:host] = "vocalvoters.com"  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'vocalvoters.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
+    :domain         => host,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
