a62d559b376953fa8ca1d8f301ed8ec042435820
lettergram <austin@agw.io>
2023-04-19 23:02:52 -0500

    changed password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
+    :password       => ENV['SENDGRID_API_KEY_VOCALVOTERS'],
     :domain         => host,
     :enable_starttls_auto => true
   }
